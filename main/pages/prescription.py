deddcece68a5fc5fa21470d5bb5b71c4464fb766
xLasercut <tianxun.wang2004@gmail.com>
2020-01-15 12:07:39 +0000

    added more inputs for prescription page

--- main/pages/prescription.py
+++ main/pages/prescription.py
@@ -11,16 +11,26 @@ from main.shared_modules.pagination import generate_pages
 
 class PrescriptionPage(Page):
     eng_name = TextField(verbose_name='英文名', default='')
+    classification = TextField(verbose_name='分类', default='')
+    source = RichTextField(verbose_name='出处', default='')
     usage = RichTextField(verbose_name='用法', default='')
     function = RichTextField(verbose_name='功用', default='')
     cures = RichTextField(verbose_name='主治', default='')
+    pathogenesis = RichTextField(verbose_name='病机', default='')
+    application = RichTextField(verbose_name='运用', default='')
+    indication = RichTextField(verbose_name='方歌', default='')
 
     mandatory_panels = [
         FieldPanel('eng_name'),
+        FieldPanel('classification'),
+        FieldPanel('source'),
         InlinePanel('composition', label='组成'),
         FieldPanel('usage'),
         FieldPanel('function'),
-        FieldPanel('cures')
+        FieldPanel('cures'),
+        FieldPanel('pathogenesis'),
+        FieldPanel('application'),
+        FieldPanel('indication')
     ]
 
     content_panels = Page.content_panels + [
@@ -36,6 +46,16 @@ class PrescriptionPage(Page):
             composition_list.append(composition.medicine_page.title)
 
         return composition_list
+
+    @property
+    def get_composition(self):
+
+        composition_images = []
+
+        for composition in self.specific.composition.all():
+            composition_images.append(composition.medicine_page)
+
+        return composition_images
 
 
 class PrescriptionComposition(Orderable):
